2e615c955b7cc7bf9e148dea91aecb08e2d6f444
cryptojang <robert.crypto.jang@gmail.com>
2023-11-22 11:26:57 +0900

    infinite scroller

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+import Box from "./components/Box";
+import DetectArea from "./components/DetectArea";
+
 const App = () => {
+  const [boxCount, setBoxCount] = useState(6);
+
+  const [boxCompList, setBoxCompList] = useState([
+    <Box boxCount={1} />,
+    <Box boxCount={2} />,
+    <Box boxCount={3} />,
+    <Box boxCount={4} />,
+    <Box boxCount={5} />,
+  ]);
+
+  const detectAreaRef = useRef();
+  const observer = useRef();
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          console.log("디텍딩 구역이 감지되었습니다");
+          setBoxCompList([
+            ...boxCompList,
+            <Box boxCount={boxCount} />,
+            <Box boxCount={boxCount + 1} />,
+            <Box boxCount={boxCount + 2} />,
+            <Box boxCount={boxCount + 3} />,
+            <Box boxCount={boxCount + 4} />,
+          ]);
+          setBoxCount(boxCount + 5);
+          console.log(boxCompList.length);
+        }
+      },
+      { threshold: 0.7 }
+    );
+
+    observer.current.observe(detectAreaRef.current);
+
+    return () => observer.current.unobserve(detectAreaRef.current);
+  }, [boxCompList]);
+
   return (
-    <div className="bg-red-100">
-      <div>Hello</div>
-    </div>
+    <ul className="flex flex-col items-center gap-20">
+      {boxCompList.map((v, i) => (
+        <li key={i}>{v}</li>
+      ))}
+
+      <DetectArea detectAreaRef={detectAreaRef} />
+    </ul>
   );
 };
 
